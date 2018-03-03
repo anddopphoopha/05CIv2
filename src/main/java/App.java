b998b28936f6fa95af1724b3658c9fbcdc74fa21
Unknown <andrew.dopphoopha@gmail.com>
2018-03-03 22:16:16 +0700

    Feature 1

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,29 +1,48 @@
 public class App {
-    public boolean isPrime(long number){
-        double max = Math.sqrt(number)+1;
-        if (number == 2){
+    public String primeFactors = "";
+
+    public boolean isPrime(long number) {
+        double max = Math.sqrt(number) + 1;
+        if (number == 2) {
             return true;
         }
-        if (number % 2 == 0){
+        if (number % 2 == 0) {
             return false;
         }
 
-        for(long i = 3; i < max; i += 2){
-            if(number %i==0){
+        for (long i = 3; i < max; i += 2) {
+            if (number % i == 0) {
                 return false;
             }
         }
         return true;
-}
+    }
 
-public static void main(String[] args) {
-   if(args.length < 1 || args.length > 1){
-       System.out.println("Please input a number as the program argument");
-       System.exit(0);
-   }
-   App appObj = new App();
-   long input = Long.parseLong(args[0]);
-   
-   System.out.println(appObj.isPrime(input));
-}
+    public void primeFactor(int num) {
+        for (int i = 2; i <= num; i++) {
+            if (num % i == 0) {
+                if (isPrime(i)) {
+                    primeFactors += i + " ";
+                }
+                primeFactor(num / i);
+                break;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        App appObj = new App();
+        if (args.length < 1 || args.length > 1) {
+            System.out.println("Please input a number as the program argument");
+            System.exit(0);
+        } else if (args.length == 1) {
+            int num1 = Integer.parseInt(args[0]);
+            appObj.primeFactor(num1);
+            System.out.println(appObj.primeFactors);
+        }
+
+        long input = Long.parseLong(args[0]);
+
+        System.out.println(appObj.isPrime(input));
+    }
 }
--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -3,24 +3,41 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class AppTest {
-@Test
-public void testSmallPrime(){
-    App appObj = new App();
-    assertEquals(appObj.isPrime(17),true);
-}
-@Test
-public void testSmallNoPrime(){
-    App appObj = new App();
-    assertEquals(appObj.isPrime(33),false);
-}
-@Test
-public void testLargePrime(){
-    App appObj = new App();
-    assertEquals(appObj.isPrime(32416189049L),true);
-}
-@Test
-public void testLargeNoPrime(){
-    App appObj = new App();
-    assertEquals(appObj.isPrime(32416189051L),false);
-}
+    @Test
+    public void testSmallPrime() {
+        App appObj = new App();
+        assertEquals(appObj.isPrime(17), true);
+    }
+
+    @Test
+    public void testSmallNoPrime() {
+        App appObj = new App();
+        assertEquals(appObj.isPrime(33), false);
+    }
+
+    @Test
+    public void testLargePrime() {
+        App appObj = new App();
+        assertEquals(appObj.isPrime(32416189049L), true);
+    }
+
+    @Test
+    public void testLargeNoPrime() {
+        App appObj = new App();
+        assertEquals(appObj.isPrime(32416189051L), false);
+    }
+
+    @Test
+    public void onePrime() {
+        App appObj = new App();
+        appObj.primeFactor(147);
+        assertTrue(appObj.primeFactors.equals("3 7 7 "));
+    }
+
+    @Test
+    public void oneNotPrime() {
+        App appObj = new App();
+        appObj.primeFactor(35);
+        assertTrue(appObj.primeFactors.equals("5 7 "));
+    }
 }

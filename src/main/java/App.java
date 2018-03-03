d06a8cdc4f237e9ebd9df876bcf1bfc42f4e8e29
Unknown <andrew.dopphoopha@gmail.com>
2018-03-03 22:27:23 +0700

    Feature 3

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,29 +1,43 @@
 public class App {
-    public boolean isPrime(long number){
-        double max = Math.sqrt(number)+1;
-        if (number == 2){
+    public String primeNumbers2 = "";
+
+    public boolean isPrime(long number) {
+        double max = Math.sqrt(number) + 1;
+        if (number == 2) {
             return true;
         }
-        if (number % 2 == 0){
+        if (number % 2 == 0) {
             return false;
         }
 
-        for(long i = 3; i < max; i += 2){
-            if(number %i==0){
+        for (long i = 3; i < max; i += 2) {
+            if (number % i == 0) {
                 return false;
             }
         }
         return true;
-}
+    }
 
-public static void main(String[] args) {
-   if(args.length < 1 || args.length > 1){
-       System.out.println("Please input a number as the program argument");
-       System.exit(0);
-   }
-   App appObj = new App();
-   long input = Long.parseLong(args[0]);
-   
-   System.out.println(appObj.isPrime(input));
-}
+    public void primeFactor(int[] num) {
+        for (int i = 0; i < num.length; i++) {
+            if (isPrime(num[i])) {
+                primeNumbers2 += (num[i] + " ");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        App appObj = new App();
+        if (args.length > 2) {
+            int[] array = new int[args.length];
+            for (int i = 0; i < args.length; i++) {
+                array[i] = Integer.parseInt(args[i]);
+            }
+            appObj.primeFactor(array);
+            System.out.println(appObj.primeNumbers2);
+        } else {
+            long input = Long.parseLong(args[0]);
+            System.out.println(appObj.isPrime(input));
+        }
+    }
 }
--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -23,4 +23,11 @@ public void testLargeNoPrime(){
     App appObj = new App();
     assertEquals(appObj.isPrime(32416189051L),false);
 }
+@Test
+    public void morePrime(){
+        App appObj = new App();
+        int[] num = {1,2,3,4,5};
+        appObj.primeFactor(num);
+        assertTrue(appObj.primeNumbers2.equals("1 2 3 5 "));
+    }
 }

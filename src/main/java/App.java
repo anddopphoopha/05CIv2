98f206d7b49739a893f74d00aa374b30777f8388
Unknown <andrew.dopphoopha@gmail.com>
2018-03-03 22:22:06 +0700

    Feature 2

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,29 +1,41 @@
 public class App {
-    public boolean isPrime(long number){
-        double max = Math.sqrt(number)+1;
-        if (number == 2){
+    public String primeNumbers1 = "";
+
+    public boolean isPrime(long number) {
+        double max = Math.sqrt(number) + 1;
+        if (number == 2) {
             return true;
         }
-        if (number % 2 == 0){
+        if (number % 2 == 0) {
             return false;
         }
 
-        for(long i = 3; i < max; i += 2){
-            if(number %i==0){
+        for (long i = 3; i < max; i += 2) {
+            if (number % i == 0) {
                 return false;
             }
         }
         return true;
-}
+    }
 
-public static void main(String[] args) {
-   if(args.length < 1 || args.length > 1){
-       System.out.println("Please input a number as the program argument");
-       System.exit(0);
-   }
-   App appObj = new App();
-   long input = Long.parseLong(args[0]);
-   
-   System.out.println(appObj.isPrime(input));
-}
+    public void countPrimeNumber(int num, int num2) {
+        for (int i = num; i <= num2; i++) {
+            if (isPrime(i)) {
+                primeNumbers1 += (i + " ");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        App appObj = new App();
+       if (args.length == 2) {
+            int num1 = Integer.parseInt(args[0]);
+            int num2 = Integer.parseInt(args[1]);
+            appObj.countPrimeNumber(num1, num2);
+            System.out.println(appObj.primeNumbers1);
+        } else {
+            long input = Long.parseLong(args[0]);
+            System.out.println(appObj.isPrime(input));
+        }
+    }
 }
--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -3,24 +3,34 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class AppTest {
-@Test
-public void testSmallPrime(){
-    App appObj = new App();
-    assertEquals(appObj.isPrime(17),true);
-}
-@Test
-public void testSmallNoPrime(){
-    App appObj = new App();
-    assertEquals(appObj.isPrime(33),false);
-}
-@Test
-public void testLargePrime(){
-    App appObj = new App();
-    assertEquals(appObj.isPrime(32416189049L),true);
-}
-@Test
-public void testLargeNoPrime(){
-    App appObj = new App();
-    assertEquals(appObj.isPrime(32416189051L),false);
-}
+    @Test
+    public void testSmallPrime() {
+        App appObj = new App();
+        assertEquals(appObj.isPrime(17), true);
+    }
+
+    @Test
+    public void testSmallNoPrime() {
+        App appObj = new App();
+        assertEquals(appObj.isPrime(33), false);
+    }
+
+    @Test
+    public void testLargePrime() {
+        App appObj = new App();
+        assertEquals(appObj.isPrime(32416189049L), true);
+    }
+
+    @Test
+    public void testLargeNoPrime() {
+        App appObj = new App();
+        assertEquals(appObj.isPrime(32416189051L), false);
+    }
+
+    @Test
+    public void twoPrime() {
+        App appObj = new App();
+        appObj.countPrimeNumber(1, 5);
+        assertTrue(appObj.primeNumbers1.equals("1 2 3 5 "));
+    }
 }
